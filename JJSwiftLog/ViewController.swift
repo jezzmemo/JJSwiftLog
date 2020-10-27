bd44e46eff7358a98e3617494ec4d76bb7ae0199
Jezz <pearjie250@163.com>
2020-10-27 23:10:02 +0800

    short name sample code

--- JJSwiftLog/ViewController.swift
+++ JJSwiftLog/ViewController.swift
@@ -15,21 +15,21 @@ class ViewController: UIViewController {
     }
     
     func setupVendor(parameter: String) {
-        jjLogger.verbose("method set the parameter")
+        JJLogger.verbose("method set the parameter")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        jjLogger.enable = true
+        JJLogger.enable = true
         
         setupLog()
         setupVendor(parameter: "Show the error log")
-        jjLogger.verbose("Start the record")
-        jjLogger.debug("Debug the world")
-        jjLogger.info("Show log info")
-        jjLogger.warning("Build warning")
-        jjLogger.error("can’t fetch user info without user id")
+        JJLogger.verbose("Start the record")
+        JJLogger.debug("Debug the world")
+        JJLogger.info("Show log info")
+        JJLogger.warning("Build warning")
+        JJLogger.error("can’t fetch user info without user id")
     
     }
 
